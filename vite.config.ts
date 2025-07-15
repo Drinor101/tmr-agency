1505152e5e41fb2b95f67a0f604954e8a634e583
drinor101 <drinorshabiu1@gmail.com>
2025-07-15 03:10:53 +0200

    fixes-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tmr-agency/', // This line is critical!
+  base: '/tmr-agency/',
 });
