3150c012d53a2173484de41a0cc782b37be7b954
drinor101 <drinorshabiu1@gmail.com>
2025-07-15 03:08:07 +0200

    dp-fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tmr-agency/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/tmr-agency/', // This line is critical!
 });
